fe33761d565648525b1c0d56b08aa800bbeb9c86
Amr Keleg <amr_mohamed@live.com>
2022-03-20 11:38:53 +0000

    Use HF's run_mlm script to test continue pretraining

---
+++ run_mlm.sh
@@ -0,0 +1,9 @@
+python run_mlm.py \
+    --model_name_or_path roberta-base \
+    --train_file path_to_train_file \
+    --validation_file path_to_validation_file \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
+    --do_train \
+    --do_eval \
+    --output_dir test-mlm/
