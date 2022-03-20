74e57384507aaddc4ea3e1819209efcc72ca0c7f
Amr Keleg <amr_mohamed@live.com>
2022-03-20 00:26:17 +0000

    Fix the baseline script

--- baseline.py
+++ baseline.py
@@ -1,16 +1,5 @@
 import json
-import os
-import re
-import sys
-
-import requests
-import string
-from colorama import Fore
-from tqdm import tqdm
-
-from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
-from settings import gpu, epochs, max_seq_length, batch_size
-from data import create_squad_examples, create_inputs_targets
+from data_utils import create_squad_examples, create_inputs_targets
 
 with open("data/eval_ar.jsonl") as f:
     raw_eval_data = [json.loads(l) for l in f]
